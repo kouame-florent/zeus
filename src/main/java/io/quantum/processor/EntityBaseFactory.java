8725b2a4a89d0432a8440e111bcc60f52590a755
Florent Kouamé <florent.skynet@gmail.com>
2019-11-12 10:36:25 +0000

    Add EntityBaseFactory

---
+++ src/main/java/io/quantum/processor/EntityBaseFactory.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.quantum.processor;
+
+import com.squareup.javapoet.JavaFile;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.type.DeclaredType;
+import javax.tools.Diagnostic;
+
+/**
+ *
+ * @author root
+ */
+public abstract class EntityBaseFactory {
+    
+    private final Filer filer;
+    private final Messager messager;
+    private static final String ENTITY_ANNOTATION_FQCN = "javax.persistence.Entity";
+    
+    public List<Element> annotatedElements = new ArrayList<>();
+    public List<Element> badAnnotatedElements = new ArrayList<>();
+
+    public EntityBaseFactory(Filer filer, Messager messager) {
+        this.filer = filer;
+        this.messager = messager;
+    }
+        
+    
+    boolean isEntity(Element element){
+        return element.getAnnotationMirrors()
+                .stream().map(AnnotationMirror::getAnnotationType)
+                .map(DeclaredType::toString)
+                .anyMatch(n -> n.equalsIgnoreCase(ENTITY_ANNOTATION_FQCN));
+    }
+    
+    abstract JavaFile buildCode(Element element);
+   
+    void generateCode(ProcessingEnvironment processingEnv){
+        annotatedElements.stream()
+            .filter(this::isEntity)
+            .map(e -> buildCode(e))
+            .forEach(jf -> writeFile(jf, processingEnv));
+    }
+    
+     public void writeFile(JavaFile javaFile,ProcessingEnvironment processingEnv){
+        try {
+            javaFile.writeTo(filer);
+        } catch (IOException ex) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,"[Zeus] File already generated! ");
+        }
+    }
+}
--- src/main/java/io/quantum/processor/EntityDaoImplFactory.java
+++ src/main/java/io/quantum/processor/EntityDaoImplFactory.java
@@ -6,44 +6,26 @@
 package io.quantum.processor;
 
 import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
-import com.squareup.javapoet.TypeVariableName;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
-import javax.lang.model.type.DeclaredType;
-import javax.tools.Diagnostic;
 
 /**
  *
  * @author root
  */
-public class EntityDaoImplFactory {
+public class EntityDaoImplFactory extends EntityBaseFactory{
     
-    private final Filer filer;
-    private final Messager messager;
-    private static final String ENTITY_ANNOTATION_FQCN = "javax.persistence.Entity";
-        
-    public List<Element> annotatedElements = new ArrayList<>();
-    public List<Element> badAnnotatedElements = new ArrayList<>();
-
     public EntityDaoImplFactory(Filer filer, Messager messager) {
-        this.filer = filer;
-        this.messager = messager;
+        super(filer, messager);
     }
-    
 
     public void add(Element element){
         annotatedElements.add(element);
@@ -66,9 +48,9 @@ public class EntityDaoImplFactory {
    private String interfaceName(String elementSimpleName){
        return elementSimpleName + "DAO" ;
    }
-   
-       
-    private JavaFile buildCode(Element element) {
+          
+    @Override
+    JavaFile buildCode(Element element) {
         
         String entityName = element.getSimpleName().toString();
         String entityDaoImplName = className(entityName);
@@ -94,33 +76,10 @@ public class EntityDaoImplFactory {
                 .addMethod(constructor)
                 .build();
         
-          
         return JavaFile.builder("io.quantum.dao.impl", entityDao)
                 .skipJavaLangImports(true)
                 .indent("    ")
                 .build();
    }
     
-    private boolean isEntity(Element element){
-        return element.getAnnotationMirrors()
-                .stream().map(AnnotationMirror::getAnnotationType)
-                .map(DeclaredType::toString)
-                .anyMatch(n -> n.equalsIgnoreCase("javax.persistence.Entity"));
-    }
-   
-   public void generateCode(ProcessingEnvironment processingEnv){
-        annotatedElements.stream()
-            .filter(this::isEntity)
-            .map(e -> buildCode(e))
-            .forEach(jf -> writeFile(jf, processingEnv));
-    }
-   
-    public void writeFile(JavaFile javaFile,ProcessingEnvironment processingEnv){
-        try {
-            
-            javaFile.writeTo(filer);
-        } catch (IOException ex) {
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,"[Zeus] File already generated! ");
-        }
-    }
 }
